ce6a9b1365876da6ad63ab6ad28fb18e93f0cbf7
Kainax001 <yoyokingg00@gmail.com>
2025-12-28 15:59:14 +0900

    fix: registered return scroll

--- src/main/java/com/kainax00/simcitymod/registry/ModItems.java
+++ src/main/java/com/kainax00/simcitymod/registry/ModItems.java
@@ -3,6 +3,7 @@ package com.kainax00.simcitymod.registry;
 import com.kainax00.simcitymod.SimcityMod;
 import com.kainax00.simcitymod.item.ChunkClaimItem;
 import com.kainax00.simcitymod.item.ChunkUnclaimItem;
+import com.kainax00.simcitymod.item.ReturnScrollItem;
 import com.kainax00.simcitymod.item.TraderSpawnEggItem;
 
 import net.minecraft.world.item.Item;
@@ -17,25 +18,37 @@ public class ModItems {
         DeferredRegister.create(ForgeRegistries.ITEMS, SimcityMod.MOD_ID);
 
     public static final RegistryObject<Item> CHUNK_CLAIMER = ITEMS.register("chunk_claimer", 
-        () -> new ChunkClaimItem(
-            new Item.Properties().setId(ITEMS.key("chunk_claimer")).stacksTo(1)
+            () -> new ChunkClaimItem(
+                new Item.Properties()
+                .setId(ITEMS.key("chunk_claimer"))
+                .stacksTo(1)
         )
     );
 
     public static final RegistryObject<Item> CHUNK_UNCLAIMER = ITEMS.register("chunk_unclaimer", 
-        () -> new ChunkUnclaimItem(
-            new Item.Properties().setId(ITEMS.key("chunk_unclaimer")).stacksTo(1)
+            () -> new ChunkUnclaimItem(
+                new Item.Properties()
+                .setId(ITEMS.key("chunk_unclaimer"))
+                .stacksTo(1)
         )
     );
 
     public static final RegistryObject<Item> TRADER_SPAWN_EGG = ITEMS.register("trader_spawn_egg",
-    () -> new TraderSpawnEggItem(
-        ModEntities.TRADER, 
-        0x543618, 
-        0x00FF00, 
-        new Item.Properties().setId(ITEMS.key("trader_spawn_egg"))
-    )
-);
+            () -> new TraderSpawnEggItem(
+                ModEntities.TRADER, 
+                0x543618, 
+                0x00FF00, 
+                new Item.Properties().setId(ITEMS.key("trader_spawn_egg"))
+        )
+    );
+
+    public static final RegistryObject<Item> RETURN_SCROLL = ITEMS.register("return_scroll",
+            () -> new ReturnScrollItem(
+                new Item.Properties()
+                .setId(ITEMS.key("return_scroll"))
+                .stacksTo(16)
+        )
+    );
 
     public static void register(BusGroup bus) {
         ITEMS.register(bus);
