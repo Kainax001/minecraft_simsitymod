7293cf11ec3e5e6da464ef05cb8bb6eb6ef94e14
Kainax001 <yoyokingg00@gmail.com>
2025-12-28 22:42:31 +0900

    fix: server console permission rogic fix

--- src/main/java/com/kainax00/simcitymod/util/PermissionUtil.java
+++ src/main/java/com/kainax00/simcitymod/util/PermissionUtil.java
@@ -4,28 +4,26 @@ import com.kainax00.simcitymod.manager.PlayerDataManager;
 import com.kainax00.simcitymod.data.enums.PermissionLevel;
 import com.kainax00.simcitymod.data.info.PlayerInfo;
 import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 
 public class PermissionUtil {
-
+    
     public static boolean hasAdminPermission(Player player) {
         if (player == null) return false;
 
         PlayerInfo info = PlayerDataManager.getPlayerData(player);
 
-        if (info != null && info.getPermissionLevel() == PermissionLevel.ADMIN) {
-            return true;
-        }
-        return false;
+        return info != null && info.getPermissionLevel() == PermissionLevel.ADMIN;
     }
 
     public static boolean hasAdminPermission(CommandSourceStack source) {
-        if (source.getEntity() == null) {
-            return true;
+        if (!source.isPlayer()) {
+            return true; 
         }
 
-        if (source.getEntity() instanceof Player player) {
-            return hasAdminPermission(player);
+        if (source.getEntity() instanceof ServerPlayer serverPlayer) {
+            return hasAdminPermission(serverPlayer);
         }
 
         return false;
